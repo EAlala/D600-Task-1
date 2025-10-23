008886d36bb0850bfb6d5ca091b948f906faa634
Edgar <yeai22@live.com>
2025-10-23 12:33:20 -0500

    Define variables

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import pandas as pd
 
-
 #Read data set
 data_set = pd.read_csv("C:/Users/yeai2_6rsknlh/OneDrive/Visual/D600 Task 1/D600 Task 1 Dataset 1 Housing Information.csv")
+
+#Define variables
+dep_var = "Price"
+indep_vars = ["SquareFootage", "NumBathrooms", "NumBedrooms", "BackyardSpace",
+    "CrimeRate", "SchoolRating", "AgeOfHome", "DistanceToCityCenter",
+    "EmploymentRate", "PropertyTaxRate", "RenovationQuality", "LocalAmenities",
+    "TransportAccess", "Fireplace", "Garage"]
+
