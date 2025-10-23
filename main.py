301c300c6051c40f609fd2e9ef9ad50b88007e07
Edgar <yeai22@live.com>
2025-10-23 14:43:00 -0500

    Final Display for stats

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 #Read data set
 data_set = pd.read_csv("C:/Users/yeai2_6rsknlh/OneDrive/Visual/D600 Task 1/D600 Task 1 Dataset 1 Housing Information.csv")
 
+
 #Define variables
 dep_var = "Price"
 indep_vars = ["SquareFootage", "NumBathrooms", "NumBedrooms", "BackyardSpace",
@@ -22,3 +23,9 @@ desc_stats["range"] = desc_stats["max"] - desc_stats["min"]
 
 #Clearer presentation
 final_stats = desc_stats[["count", "mean", "mode", "std", "min", "max", "range"]]
+
+#Display
+print(final_stats.round(2))
+
+print(f"\n{analysis_data_set["Fireplace"].value_counts()}")
+print(f"\n{analysis_data_set["Garage"].value_counts()}")
